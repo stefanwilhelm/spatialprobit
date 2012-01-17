75ed510befc6f45463eb59da193050fa74659b96
stefan <>
2012-01-17 23:52:15 +0000

    *** empty log message ***

--- R/SpatialProbit-MCMC.R
+++ R/SpatialProbit-MCMC.R
@@ -82,6 +82,62 @@ draw_rho <- function(detval,epe0,eped,epe0d,n,k,rho,a1,a2){
   return( results )
 }
 
+# PURPOSE:
+# ---------------------------------------------------
+#  USAGE: 
+#
+#  results.rmin 
+#  results.rmax maximum eigen value
+#  results.time execution time
+# ---------------------------------------------------
+#where epe0 <- t(e0) %*% e0
+#      eped <- t(ed) %*% ed
+#      epe0d<- t(ed) %*% e0
+#
+# @param detval1         # vector, detval[,1]
+# @param detval2         # vector, detval[,2]
+# @param detval1sq       # detval1 * detval1
+# @param epe0            # skalar
+# @param eped            # skalar
+#
+# @param nmk = (n-k)/2
+# @param nrho = nrow(detval), defaults to nrho=2001
+# @param lnbprior = log(beta_prior(detval[,1],a1,a2))
+# @param u ~ Uni(0, 1)
+draw_rho2 <- function(detval1, detval2, detval1sq, yy, epe0, eped, epe0d, rho, nmk, nrho, lnbprior, u){
+  #nmk     <- (n-k)/2        # SW: does not depend on z, rho
+  #nrho    <- nrow(detval)   # SW: does not depend on z, rho
+  #iota    <- ones(nrho,1)   # SW: does not depend on z, rho : does it only create a vector with ones? (2001 x 1)
+  #LeSage 2009 page 132 justifies the code below
+  #detval1      <- detval[,1]  # SW: avoid multiple accesses to detval[,1]
+  #detval2      <- detval[,2]
+  z       <- epe0 - 2 * detval1 * epe0d + detval1sq * eped #CHECK epe %*% vs * # SW: 2001 x 1 # detval1 * detval1 can be precalculated
+  z       <- -nmk * log(z)
+  #C = gammaln(nmk)*iota -nmk*log(2*pi)*iota - 0.5*logdetx*iota;
+  den     <- detval2 + z + lnbprior
+  #bprior  <- beta_prior(detval[,1],a1,a2)   # SW: does not depend on z, rho, draws one sample from beta distribution???
+  #den     <- den + lnbprior                  # SW: log(bprior) does not depend on z, rho
+  #n       <- length(den)                    # SW: Is this n different from method argument? I.e. is it necessary?
+  n       <- nrho                            # SW!
+  #y       <- detval1                         # SW: does not depend on z, rho
+  adj     <- max(den)
+  den     <- den - adj
+  x       <- exp(den)
+  # trapezoid rule used for numerical integral approximation
+  isum    <- sum(yy * (x[2:n] - x[1:(n-1)])/2) # (y[2:n] + y[1:(n-1)]) does not depend on z
+  z       <- abs(x/isum)
+  den     <- cumsum(z)
+  rnd     <- u * sum(z)     # SW: runif can be precalculated
+  ind     <- which( den <= rnd ) # SW: Suche max. index, wo den noch kleiner ist als rnd
+  idraw   <- max(ind) #returns -inf if the ind lis is empty
+  if (  idraw > 0 && idraw < nrho ){
+    results <- detval1[idraw]
+  }else{
+    results <- rho
+  }
+  return( results )
+}
+
 # Estimate the spatial probit model 
 # z = rho * W + X \beta + epsilon
 # where y = 1 if z >= 0 and y = 0 if z < 0 observable
@@ -91,7 +147,7 @@ draw_rho <- function(detval,epe0,eped,epe0d,n,k,rho,a1,a2){
 # @param W spatial weight matrix
 # @param ndraw number of MCMC iterations
 # @param burn.in  number of MCMC burn-in to be discarded
-# @param thinning thinning factor, defaults to 1
+# @param thinning MCMC thinning factor, defaults to 1
 sar_probit_mcmc <- function(y, X, W, ndraw=1000, burn.in=100, thinning=1, prior, start=list(rho=0.75, beta=rep(0, ncol(X)))){  
 
   n <- nrow( X )             # number of observations
@@ -116,6 +172,19 @@ sar_probit_mcmc <- function(y, X, W, ndraw=1000, burn.in=100, thinning=1, prior,
   ldetflag   <-  0   # default to 1999 Pace and Barry MC determinant approx
   tmp <- sar_lndet(ldetflag, W, rmin, rmax)
   detval <- tmp$detval
+  # SW: Some precalculated quantities for drawing rho
+  # rho ~ Beta(a1, a2) prior
+  a1         <-  1.0
+  a2         <-  1.0
+  lnbprior <- log(beta_prior(detval[,1],a1,a2))
+  u        <- runif(thinning * ndraw + burn.in)   # u ~ U(0, 1)
+  nrho     <- 2001
+  nmk      <- (n-k)/2
+  detval1  <- detval[,1]  # SW: avoid multiple accesses to detval[,1]
+  detval2  <- detval[,2]
+  detval1sq <- detval1 * detval1
+  yy        <- (detval1[2:nrho] + detval1[1:(nrho-1)])
+
   
   # matrix to store the beta + rho parameters for each iteration/draw
   B <- matrix(NA, ndraw, k+1)
@@ -124,9 +193,12 @@ sar_probit_mcmc <- function(y, X, W, ndraw=1000, burn.in=100, thinning=1, prior,
   pb <- txtProgressBar(min=0, max=(thinning * ndraw + burn.in), initial=0, style=3)
   
   # immutable matrices
+  tX <- t(X)                       # X'
   xpx  <- t(X) %*% X               # (X'X)
-  xpxI <- solve(t(X) %*% X)        # (X'X)^{-1}
-  AA   <- solve(t(X) %*% X + Tinv) # (X'X + T^{-1})^{-1}
+  xpxI <- solve(xpx)               # (X'X)^{-1}
+  #xxpxIxp <- X %*% xpxI %*% tX    # X(X'X)^(-1)X'    # n x n (argh!)
+  xxpxI    <- X %*% xpxI           # X(X'X)^(-1)     # n x k (better, compromise)
+  AA       <- solve(xpx + Tinv)    # (X'X + T^{-1})^{-1}
   
   # draw from multivariate normal beta ~ N(c, T). we can precalculate 
   # betadraws ~ N(0, T) befor running the chain and later just create beta as
@@ -138,49 +210,53 @@ sar_probit_mcmc <- function(y, X, W, ndraw=1000, burn.in=100, thinning=1, prior,
   # 1. sample from z | rho, beta, y using precision matrix H
   # solving equation (I_n - rho * W) mu = X beta instead of  inverting S = I_n - rho * W
   # as in mu = ( In -  rho W)^{-1} X beta
-  mu <- qr.solve(S, X %*% beta)
+  # do not try to use neither this slow version
+  # mu <- qr.solve(S, X %*% beta)
+  # nor this memory-consuming versions
+  # mu <- solve(S) %*% X %*% beta
+  # QR-decomposition for sparse matrices
+  QR <- qr(S)  # class "sparseQR"
+  mu <- solve(QR, X %*% beta)
   
-  # we have to supress check for positive-definiteness of H in rtmvnorm()
   # see LeSage (2009) for choice of burn-in size, often m=10 is used!
-  z <- as.double(rtmvnorm.sparseMatrix(n=1, mean=mu, H=H,
-    lower=lower, upper=upper, burn.in=20))
-  #z <- as.double(rtmvnorm.sparseMatrix(n=1, mean=rep(0, n), H=H,
-  #  lower=lower - mu, upper=upper - mu, burn.in=20))  
-    
+  # TODO: we can also use m=1 together with start.value=z, see LeSage (2009), section 10.1.5
+  z <- as.double(rtmvnorm.sparseMatrix(n=1, mean=mu, H=H, 
+    lower=lower, upper=upper, burn.in=1, start.value=as.double(z)))
+  #z <- as.double(rtmvnorm.gibbs.Fortran(n=1, mean=mu, H=H, 
+  #  lower=lower, upper=upper, burn.in=10))
     
   # 2. sample from beta | rho, z, y
-  c <- AA  %*% (t(X) %*% S %*% z + Tinv %*% c)
-  T <- AA
+  c <- AA  %*% (tX %*% S %*% z + Tinv %*% c)
+  T <- AA   # no update basically on T, TODO: check this
   beta <- as.double(c + betadraws[i + burn.in, ])
   
   # 3. sample from rho | beta, z
-  
-  # rho ~ Beta(a1, a2) prior
-  a1         <-  1.0 
-  a2         <-  1.0 
-  
   #---- DRAW RHO ----
   #see LeSage 2009 chapter 5 - page 132 for the explanation of the
   #code below which is used for numerical integration of the rho prior.
   #I changed from the original code to match the notation of the book
   #using c0 and cd below instead of b0 and bd ....
-  xpz  <- t(X) %*% z        # X'z
-  xpWz <- t(X) %*% W %*% z  # X'Wz
-  Wz   <- W %*% z           # Wz
-  c0   <-  xpxI %*% xpz     #(X'X)^-1 X'z
-  cd   <-  xpxI %*% xpWz    #(X'X)^(-1) * X'Wz
-  e0   <-  z - X %*% c0
-  ed   <- Wz - X %*% cd
-  epe0 <- as.real(crossprod(e0))  # slightly faster than t(e0) %*% e0 
-  eped <- as.real(crossprod(ed))
-  epe0d<- as.real(t(ed) %*% e0)    
-  rho  <- draw_rho(detval,epe0,eped,epe0d,n,k,rho,a1,a2)
+  xpz  <- tX %*% z           # X'z
+  Wz   <- as.double(W %*% z) # Wz        # SW: coerce Wz to vector 
+  # (from n x 1 sparse matrix! we do not need a sparse matrix here)
+  xpWz <- tX %*% Wz          # X'Wz      # k x 1
+  #c0   <-  xpxI %*% xpz     #(X'X)^-1 X'z
+  #cd   <-  xpxI %*% xpWz    #(X'X)^(-1) * X'Wz
+  #e0   <-  z - X %*% c0      # z  - X(X'X)^-1X' z
+  #ed   <- Wz - X %*% cd      # Wz - X(X'X)^(-1)X'Wz
+  # SW: tried to avoid some matrix multiplications here, dropped c0 and d0
+  e0   <-  z - xxpxI %*% xpz  # z  - X(X'X)^-1X' z
+  ed   <- Wz - xxpxI %*% xpWz # Wz - X(X'X)^(-1)X'Wz
+  epe0 <- as.double(crossprod(e0))  # slightly faster than t(e0) %*% e0
+  eped <- as.double(crossprod(ed))
+  epe0d<- as.double(crossprod(ed, e0))
+  rho  <- draw_rho2(detval1,detval2, detval1sq, yy, epe0, eped, epe0d, rho, nmk=nmk, nrho=2001, lnbprior, u=u[i + burn.in])
   
   ############################################################################## 
   
   # update S and H
   S <- I_n - rho * W
-  H <- t(S) %*% S
+  H <- t(S) %*% S      # H = S'S  / SW: crossprod(S) does not seem to work!
   
   if (i > 0) {
     if (thinning == 1) {
@@ -198,7 +274,7 @@ sar_probit_mcmc <- function(y, X, W, ndraw=1000, burn.in=100, thinning=1, prior,
   results       <- NULL
   results$nobs  <- n          # number of observations
   results$nvar  <- k          # number of explanatory variables
-  results$y     <- y          # response variable
+  results$y     <- y 
   results$zip   <- n - sum(y) # number of zero values in the y-vector
   results$beta  <- colMeans(B)[1:k]
   results$rho   <- colMeans(B)[k+1]
@@ -369,13 +445,12 @@ I_n <- sparseMatrix(i=1:n, j=1:n, x=1)
 print(object.size(I_n), units="Mb")
   
 # build spatial weight matrix W from coordinates in X
-W <- buildSpatialWeightMatrix(X[,c("x","y")], m=6)
+W <- buildSpatialWeightMatrix(cbind(x=rnorm(n), y=rnorm(n)), m=6)
 #W <- sparseMatrix(i = 1:n, j=1:n, x=1) # identity matrix for testing  
   
 # create samples from epsilon using independence of distributions (rnorm()) to avoid dense matrix I_n
 eps <- rnorm(n=n, mean=0, sd=1)
-S <- solve(I_n - rho * W)
-z <- S %*% X %*% beta + S %*% eps
+z <- solve(qr(I_n - rho * W), X %*% beta + eps)
 y <- as.vector(z >= 0)  # binary observables, 0 or 1, FALSE or TRUE
 
 # MCMC estimation of spatial probit
@@ -384,11 +459,21 @@ results <- sar_probit_mcmc(y, X, W, ndraw=1000, burn.in=100, thinning=3)
 Rprof(NULL)
 summaryRprof("sar_probit_mcmc.out")
 
+# visualization of sparsity pattern in W and H = (I_n - rho * W)'(I_n - rho * W)
+print(image(W, main = "image(W)")) # print(.) needed for Sweave
+S <- (I_n - rho * W)
+print(image(S, main = "image(I_n - rho * W)")) # print(.) needed for Sweave
+H <- t(S) %*% S
+print(image(H, main = "image(H)")) # print(.) needed for Sweave
+Sigma <- solve(H)
+print(image(Sigma, main = "image(Sigma)")) # print(.) needed for Sweave
+
+
 # compiled MCMC function
-Rprof("sar_probit_mcmc_comp.out")
-results <- sar_probit_mcmc_comp(y, X, W, ndraw=1000, burn.in=100, thinning=3)
-Rprof(NULL)
-summaryRprof("sar_probit_mcmc_comp.out")
+#Rprof("sar_probit_mcmc_comp.out")
+#results <- sar_probit_mcmc_comp(y, X, W, ndraw=1000, burn.in=100, thinning=3)
+#Rprof(NULL)
+#summaryRprof("sar_probit_mcmc_comp.out")
 
 
   
@@ -398,6 +483,9 @@ colMeans(B)
  
 # standard errors for beta and rho           
 apply(B, 2, sd)
+
+cbind(estimates=colMeans(B), SE=apply(B, 2, sd))
+
   
 # diagnostic plots for results (trace plots, ACF, posterior density function)
 getParamName <- function(i) {
