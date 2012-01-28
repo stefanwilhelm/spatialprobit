6ede246afd0647a9a23bd243950f8738a68c5c91
stefan <>
2012-01-28 22:47:01 +0000

    Changes: # added showProgress=FALSE as default. # added c.sarprobit() method SW

--- R/SpatialProbit-MCMC.R
+++ R/SpatialProbit-MCMC.R
@@ -40,46 +40,32 @@ buildSpatialWeightMatrix <- function(X, m=6) {
   return(W)
 }
 
-# PURPOSE:
-# ---------------------------------------------------
-#  USAGE: 
+# estimated tr(W^i) for i=1,...,100
+# see LeSage (2009), chapter 4, p.97/98
 #
-#  results.rmin 
-#  results.rmax maximum eigen value
-#  results.time execution time
-# ---------------------------------------------------
-#where epe0 <- t(e0) %*% e0
-#      eped <- t(ed) %*% ed
-#      epe0d<- t(ed) %*% e0
-draw_rho <- function(detval,epe0,eped,epe0d,n,k,rho,a1,a2){
-  nmk     <- (n-k)/2
-  nrho    <- nrow(detval)
-  iota    <- ones(nrho,1)
-  #LeSage 2009 page 132 justifies the code below
-  z       <-  epe0 * iota - 2*detval[,1] * epe0d + (detval[,1] * detval[,1]) * eped #CHECK epe %*% vs *
-  z       <- -nmk * log(z)
-  #C = gammaln(nmk)*iota -nmk*log(2*pi)*iota - 0.5*logdetx*iota;
-  den     <- detval[,2] + z
-  bprior  <- beta_prior(detval[,1],a1,a2)
-  den     <- den + log(bprior)
-  n       <- length(den)
-  y       <- as.matrix( detval[ ,1] )
-  adj     <- max(den)
-  den     <- den - adj
-  x       <- exp(den)
-  # trapezoid rule used for numerical integral approximation
-  isum    <- sum((y[2:n,1] + y[1:(n-1),1]) * (x[2:n,1] - x[1:(n-1),1])/2) 
-  z       <- abs(x/isum)
-  den     <- cumsum(z)
-  rnd     <- runif(1,min=0,max=1)*sum(z)
-  ind     <- which( den <= rnd )
-  idraw   <- max(ind) #returns -inf if the ind lis is empty
-  if (  idraw > 0 && idraw < nrho ){
-    results <- detval[idraw,1]
-  }else{
-    results <- rho
+# "The expectation of the quadratic form u'Au equals tr(A).
+#  Since u_i^2 follows a chi^2 distribution with one degree of freedom."
+# Pre-calculate traces tr(W^i) i=1,...,100 for the x-impacts calculations
+# TODO: SW Optimize
+#
+# @param W spatial weight matrix (n x n)
+# @param o highest order of W^i = W^o
+# @param iiter number of MCMC iterations (we run this 50 times)
+# @return (n x o) matrix with tr(W^i) in each column, for i=1..,o
+tracesWi <- function(W, o=100, iiter=50) {
+  trW_i <- matrix( data=0, nrow=n, ncol=o )   # n x 100
+  for( iii in 1:iiter ){
+    u        <- rnorm(n)         #u    -> n x 1      # randn() aus Paket pracma
+    wumat    <- matrix(0, n, o)  #wumat-> n x 100
+    wumat[,1]<-u             
+    wu       <-u                 #wu    -> n x 1
+    for(i in 2:o ){
+      wu         <- W %*% wu     # (n x n) * (n x 1) = (n x 1) matrix
+      wumat[ ,i] <- as.double(wu)
+    }
+    trW_i        <- trW_i + u * wumat  # n x 100    u' W^i u  (i = 1..100)
   }
-  return( results )
+  trW_i <- trW_i / iiter
 }
 
 # PURPOSE:
@@ -93,49 +79,29 @@ draw_rho <- function(detval,epe0,eped,epe0d,n,k,rho,a1,a2){
 #where epe0 <- t(e0) %*% e0
 #      eped <- t(ed) %*% ed
 #      epe0d<- t(ed) %*% e0
-#
-# @param detval1         # vector, detval[,1]
-# @param detval2         # vector, detval[,2]
-# @param detval1sq       # detval1 * detval1
-# @param epe0            # skalar
-# @param eped            # skalar
-#
-# @param nmk = (n-k)/2
-# @param nrho = nrow(detval), defaults to nrho=2001
-# @param lnbprior = log(beta_prior(detval[,1],a1,a2))
-# @param u ~ Uni(0, 1)
-draw_rho2 <- function(detval1, detval2, detval1sq, yy, epe0, eped, epe0d, rho, nmk, nrho, lnbprior, u){
-  #nmk     <- (n-k)/2        # SW: does not depend on z, rho
-  #nrho    <- nrow(detval)   # SW: does not depend on z, rho
-  #iota    <- ones(nrho,1)   # SW: does not depend on z, rho : does it only create a vector with ones? (2001 x 1)
-  #LeSage 2009 page 132 justifies the code below
-  #detval1      <- detval[,1]  # SW: avoid multiple accesses to detval[,1]
-  #detval2      <- detval[,2]
-  z       <- epe0 - 2 * detval1 * epe0d + detval1sq * eped #CHECK epe %*% vs * # SW: 2001 x 1 # detval1 * detval1 can be precalculated
-  z       <- -nmk * log(z)
-  #C = gammaln(nmk)*iota -nmk*log(2*pi)*iota - 0.5*logdetx*iota;
-  den     <- detval2 + z + lnbprior
-  #bprior  <- beta_prior(detval[,1],a1,a2)   # SW: does not depend on z, rho, draws one sample from beta distribution???
-  #den     <- den + lnbprior                  # SW: log(bprior) does not depend on z, rho
-  #n       <- length(den)                    # SW: Is this n different from method argument? I.e. is it necessary?
-  n       <- nrho                            # SW!
-  #y       <- detval1                         # SW: does not depend on z, rho
-  adj     <- max(den)
-  den     <- den - adj
-  x       <- exp(den)
-  # trapezoid rule used for numerical integral approximation
-  isum    <- sum(yy * (x[2:n] - x[1:(n-1)])/2) # (y[2:n] + y[1:(n-1)]) does not depend on z
-  z       <- abs(x/isum)
-  den     <- cumsum(z)
-  rnd     <- u * sum(z)     # SW: runif can be precalculated
-  ind     <- which( den <= rnd ) # SW: Suche max. index, wo den noch kleiner ist als rnd
-  idraw   <- max(ind) #returns -inf if the ind lis is empty
-  if (  idraw > 0 && idraw < nrho ){
-    results <- detval1[idraw]
-  }else{
-    results <- rho
-  }
-  return( results )
+draw_rho <- function (detval1, detval2, detval1sq, yy, epe0, eped, epe0d, 
+    rho, nmk, nrho, lnbprior, u) 
+{
+    z <- epe0 - 2 * detval1 * epe0d + detval1sq * eped
+    z <- -nmk * log(z)
+    den <- detval2 + z + lnbprior
+    n <- nrho
+    adj <- max(den)
+    den <- den - adj
+    x <- exp(den)
+    isum <- sum(yy * (x[2:n] - x[1:(n - 1)])/2)
+    z <- abs(x/isum)
+    den <- cumsum(z)
+    rnd <- u * sum(z)
+    ind <- which(den <= rnd)
+    idraw <- max(ind)
+    if (idraw > 0 && idraw < nrho) {
+        results <- detval1[idraw]
+    }
+    else {
+        results <- rho
+    }
+    return(results)
 }
 
 # Estimate the spatial probit model 
@@ -151,14 +117,36 @@ draw_rho2 <- function(detval1, detval2, detval1sq, yy, epe0, eped, epe0d, rho, n
 # @param m number of burn.in sampling in inner Gibbs sampling
 sar_probit_mcmc <- function(y, X, W, ndraw=1000, burn.in=100, thinning=1, 
   prior=list(a1=1, a2=1, c=rep(0, ncol(X)), T=diag(ncol(X))*1e12), start=list(rho=0.75, beta=rep(0, ncol(X))),
-  m=10){  
+  m=10, showProgress=FALSE){  
 
   #start timer
   timet <- Sys.time()
   
-  n <- nrow( X )             # number of observations
+  n  <- nrow( X )            # number of observations
+  n1 <- nrow( X )          
+  n2 <- nrow( W )
   k <- ncol( X )             # number of of parameters/exogenous variables
   I_n <- sparseMatrix(i=1:n, j=1:n, x=1) # sparse identity matrix
+  
+  
+  #validate inputs
+  if( length(c(which(y == 0 ),which(y == 1))) != length( y ) ){
+    stop('sarp_g: not all y-values are 0 or 1')
+  }
+  if( n1 != n2 && n1 != n ){
+    stop('sarp_g: wrong size of spatial weight matrix W')
+  }
+  # check if we have a constant term in X
+  ind <- match( n, apply(X,2,sum))
+  if( is.na(ind) ){
+    cflag <- 0
+    p     <- k
+  }else if( ind == 1 ){
+    cflag <- 1
+    p     <- k - 1
+  }else{
+    stop('sarp_g: intercept term must be in first column of the X-matrix')
+  }
   
   # MCMC sampling of beta
   rho  <- start$rho          # start value of row
@@ -203,7 +191,9 @@ sar_probit_mcmc <- function(y, X, W, ndraw=1000, burn.in=100, thinning=1,
   B <- matrix(NA, ndraw, k+1)
   
   # progress bar
-  pb <- txtProgressBar(min=0, max=(thinning * ndraw + burn.in), initial=0, style=3)
+  if (showProgress) {
+    pb <- txtProgressBar(min=0, max=(thinning * ndraw + burn.in), initial=0, style=3)
+  }
   
   # immutable matrices
   tX <- t(X)                       # X'               # k x n
@@ -213,10 +203,21 @@ sar_probit_mcmc <- function(y, X, W, ndraw=1000, burn.in=100, thinning=1,
   xxpxI    <- X %*% xpxI           # X(X'X)^(-1)     # n x k (better, compromise)
   AA       <- solve(xpx + Tinv)    # (X'X + T^{-1})^{-1}
   
+  # Monte Carlo estimation of tr(W^i) for i = 1..o
+  trW.i <- tracesWi(W, o=100, iiter=50)
+  
   # draw from multivariate normal beta ~ N(c, T). we can precalculate 
   # betadraws ~ N(0, T) befor running the chain and later just create beta as
   # beta = c + betadraws ~ N(c, T)
   betadraws <- rmvnorm(n=(burn.in + ndraw * thinning), mean=rep(0, k), sigma=AA)
+  
+  # matrices for direct and indirect impacts
+  direct       <- matrix(NA, ndraw-burn.in,p)    # n x p
+  indirect     <- matrix(NA, ndraw-burn.in,p)    # n x p
+  total        <- matrix(NA, ndraw-burn.in,p)    # n x p
+  avg_total    <- rep(0,p)                       # p x 1
+  avg_direct   <- rep(0,p)                       # p x 1
+  avg_indirect <- rep(0,p)                       # p x 1
   
   # just to set a start value for z
   z <- rep(0, n)
@@ -265,25 +266,65 @@ sar_probit_mcmc <- function(y, X, W, ndraw=1000, burn.in=100, thinning=1,
   epe0 <- as.double(crossprod(e0))  # slightly faster than t(e0) %*% e0
   eped <- as.double(crossprod(ed))
   epe0d<- as.double(crossprod(ed, e0))
-  rho  <- draw_rho2(detval1,detval2, detval1sq, yy, epe0, eped, epe0d, rho, nmk=nmk, nrho=2001, lnbprior, u=u[i + burn.in])
+  rho  <- draw_rho(detval1,detval2, detval1sq, yy, epe0, eped, epe0d, rho, nmk=nmk, nrho=2001, lnbprior, u=u[i + burn.in])
   
   ############################################################################## 
   
   # update S and H
   S <- I_n - rho * W
   H <- t(S) %*% S      # H = S'S  / SW: crossprod(S) does not seem to work!
-  
+
   if (i > 0) {
     if (thinning == 1) {
-      B[i,] <- c(beta, rho)
+      ind <- i
     }
     else if (i%%thinning == 0) {
-      B[i%/%thinning,] <- c(beta, rho)
+      ind <- i%/%thinning
+    } else {
+      next
     }
+    
+    B[ind,] <- c(beta, rho)
+  
+    # compute effects estimates (direct and indirect impacts) in each MCMC iteration
+    o <- 100
+    rhovec <- rho^(0:(o-1)) # SW: (100 x 1)   mit [1, rho^1, rho^2 ..., rho^99], see LeSage(2009), eqn (4.145), p.115
+    if( cflag == 1 ){ #has intercept
+      beff <- beta[-1]      # beff is parameter vector without constant
+    }else if(cflag == 0){
+      beff <- beta          # no constant in model
+    }
+  
+    # SW: beff is parameter vector without constant!
+    # SW: Do we have to compute impacts in every MCMC round or just once at the end?
+    # SW: See LeSage (2009), section 5.6.2., p.149 for spatial effects estimation in MCMC
+    # SW: See LeSage (2009), section 10.1.6, p.293 for Marginal effects in SAR probit
+    #hhI   <- qr.solve(I_n - rho * W)       # SW: ist das nicht eine dense matrix!?? Oder nur ein Vektor?
+    #s     <- hhI %*% I_n                   # SW: warum mit In multiplizieren? s = (In - rho * W)^{-1} !?
+    # TODO: s is a dense matrix!!!
+    pdfz      <- matrix(dnorm(as.double(mu)), ncol=1)  # standard normal pdf
+    #dd        <- sparseMatrix(i=1:n,j=1:n, x=as.double(pdfz))         # dd is a sparse diagonal matrix (n x n)
+    
+    dir      <- as.real(t(pdfz) %*% trW.i %*% rhovec /n)
+    for(r in 1:p ){
+      # direct impact : dy_i / d X_ir
+      avg_direct[r]     <- dir * beff[r]
+      #tmp               <- apply( dd %*% s * beff[r], 2, sum )  # phi(mu) wird auf die Diagonalelemente draufmultipliziert...
+      #avg_total[r]      <- mean( tmp ) 
+      #total_obs[ ,r]    <- total_obs[,r] + tmp 
+      #avg_indirect[r]   <- avg_total[r] - avg_direct[r]
+    }  
+    #total[ind, ]      <- avg_total    # an (ndraw-nomit x p) matrix
+    direct[ind, ]     <- avg_direct   # an (ndraw-nomit x p) matrix
+    #indirect[ind, ]   <- avg_indirect # an (ndraw-nomit x p) matrix
+  
+  ##############################################################################
+    
   }
-  setTxtProgressBar(pb, i + burn.in)
+  if (showProgress) setTxtProgressBar(pb, i + burn.in) # update progress bar
   }
-  close(pb) #close progress bar
+    
+  if (showProgress)  close(pb) #close progress bar
   
   # result
   results       <- NULL
@@ -308,6 +349,10 @@ sar_probit_mcmc <- function(y, X, W, ndraw=1000, burn.in=100, thinning=1,
   results$B         <- B        # (beta, rho) draws
   results$bdraw     <- B[,1:k]  # beta draws
   results$pdraw     <- B[,k+1]  # rho draws
+  results$total     <- total
+  results$direct    <- direct
+  results$indirect  <- indirect
+
   #results$predicted <- # prediction required. The default is on the scale of the linear predictors
   results$fitted.values <- NULL
   class(results)    <- "sarprobit"
@@ -331,8 +376,7 @@ summary.sarprobit <- function(object, var_names=NULL, file=NULL, ...){
   #bayesian estimation
   bout_mean <- object$coefficients                         #parameter mean column
   bout_sd   <- apply(draws, 2, sd)                         #parameter sd colum
-  #build bayesian significance levels
-  #SW: Für alle Parameter > 0: P(X >= 0)
+  # build bayesian significance levels
   # for parameter > 0 count all draws > 0  and determine P(X <= 0)
   # for parameter <= 0 count all draws <0  and determine P(X >= 0)
   bout_sig <- 1 - apply(draws, 2, function(x) { ifelse (mean(x) > 0, sum(x > 0), sum(x < 0)) }) / ndraw
@@ -365,6 +409,26 @@ summary.sarprobit <- function(object, var_names=NULL, file=NULL, ...){
   printCoefmat(coefficients, digits = max(3, getOption("digits") - 3),
     signif.stars = getOption("show.signif.stars"))       
 }
+
+# c.sarprobit works in the same way as boot:::c.boot().
+c.sarprobit <- function(...) {
+ args <- list(...)
+ nm <- lapply(args, names)
+ if (!all(sapply(nm, function(x) identical(x, nm[[1]]))))
+   stop("arguments are not all the same type of \"sarprobit\" object")
+ res <- args[[1]]
+ res$time  <- as.difftime(max(as.numeric(sapply(args, "[[", "time"), units="secs")), units="secs") 
+ res$ndraw <- sum(sapply(args, "[[", "ndraw")) 
+ res$nomit <- sum(sapply(args, "[[", "nomit"))
+ res$B     <- do.call(rbind, lapply(args, "[[", "B"))
+ res$bdraw <- do.call(rbind, lapply(args, "[[", "bdraw"))
+ res$pdraw <- do.call(rbind, lapply(args, "[[", "pdraw"))
+ res$coefficients <- colMeans(res$B) 
+ res$beta  <- res$coefficients[1:res$nvar]
+ res$rho   <- res$coefficients[res$nvar+1]
+ res
+}
+
 
 # extract the coefficients
 coef.sarprobit <- function(object, ...) {
@@ -533,3 +597,38 @@ plot(results)
 dev.off()
 }
 
+if (FALSE) {
+################################################################################
+#
+# Debug method for tr(W^i)
+#
+################################################################################
+
+# generate random samples from true model
+n <- 200             # number of items
+beta <- c(0, -1, 1)  # true model parameters k=3 beta=(beta1,beta2,beta3)
+rho <- 0.75
+# design matrix with two standard normal variates as "coordinates"
+X <- cbind(intercept=1, x=rnorm(n), y=rnorm(n))
+  
+# identity matrix I_n
+I_n <- sparseMatrix(i=1:n, j=1:n, x=1)
+print(object.size(I_n), units="Mb")
+  
+# build spatial weight matrix W from coordinates in X
+W <- buildSpatialWeightMatrix(cbind(x=rnorm(n), y=rnorm(n)), m=6)
+
+library(pracma)
+Rprof("trW.out")
+traceW.i <- tracesWi(W, o=100)
+Rprof(NULL)
+summaryRprof("trW.out")
+
+
+################################################################################
+
+undebug(sar_probit_mcmc)
+debug(sar_probit_mcmc)
+results <- sar_probit_mcmc(y, X, W, ndraw=1000, burn.in=0, thinning=1)
+results <- sar_probit_mcmc(y, X, W, ndraw=1000, burn.in=0, thinning=1, showProgress=TRUE)
+}
