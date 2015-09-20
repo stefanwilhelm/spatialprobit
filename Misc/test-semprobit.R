7f8598d804fc6b22025fff79ab843f4e690ff66b
stefan <>
2015-09-20 08:59:06 +0000

    *** empty log message ***

--- Misc/test-semprobit.R
+++ Misc/test-semprobit.R
@@ -1,47 +1,77 @@
-library(Matrix)
-library(tmvtnorm)
+library(spatialprobit)
+source("G:/R/spatialprobit/R/rtnorm.R")
+
+# SEM Probit zu Fuß: UNtersuche nur mal das Sampling z_i | z_{-i}, beta, rho, sige
+
+### Model Setup
+n <- 5
+X <- matrix(cbind(1, rnorm(n)), ncol=2)
+rho <- 0.3
+beta <- c(1, -1)
+sige <- 1.2
+I_n  <- sparseMatrix(i=1:n, j=1:n, x=1)
+W <- bandSparse(5, k=c(-1, 1), diag=list(rep(1, 5), rep(1, 5)))
+W <- W / rowSums(W)
+ones <- rep(1, n)
+W2diag <- diag(t(W)%*%W)
+
+S    <- (I_n-rho*W)
+AI   <- solve(I_n-rho*W)
+z    <- X%*%beta + AI %*% rnorm(n) * sqrt(sige)
+y    <- as.numeric(z >= 0)
+
+ind0 <- which(y == 0)      # 0 obs.
+ind1 <- which(y == 1)      # 1 obs.
+
+# truncation points for z, depend only on y, can be precalculated
+lower <- ifelse(y > 0, 0,  -Inf)
+upper <- ifelse(y > 0, Inf,   0)
 
 
-# Teste SEM model: (n=2)
-# z = X beta + (I_n-rho*W)*eps, eps ~ N(0, sige^2 * I_n)
-n <- 2
-I_n <- sparseMatrix(i=1:n, j=1:n, x=1)
-rho <- 0.5
-W <- matrix(c(0.5, 0.5,
-              0.5, 0.5), 2, 2)
-sige <- 2
-A <- (I_n - rho * W)
-AI <- solve(A)
-X <- matrix(1, 2, 1)
-beta <- 0
-z <- X%*%beta + solve(A) %*% (sige*rnorm(n=2, sd=1))
-#--> Modell führt zu einer Korrelation der Residuen von 0.6!
 
-# simuliere N Wdh. aus dem Modell z = Xbeta + (I_n-rho*W)^(-1)*eps, eps ~ N(0, sige^2 * I_n)
-N <- 10000
-eps <- matrix(rnorm(n*N), N, n)   # (N x n)
-Z <- as.matrix(sige * eps %*% AI)
-plot(Z[,1], Z[,2])
-cor(Z)            # cor=0.5748
+### Estimate z_i | z_{-i}, beta, rho, sige
 
-# simuliere direkt aus der Verteilung z ~ N(Xbeta, sige^2*AI'AI)
-Sigma <- as.matrix(sige^2*t(AI)%*%AI)
-Z2 <- rmvnorm(n=10000, mean=X%*%beta, sigma=Sigma)
-points(Z2, col="red")
-cor(Z2)
+H <- 1/sige * t(S) %*% S
+diag(H)
+1/sige * (diag(I_n) + rho^2*W2diag)
 
-# Teste Truncation:  Y = Z > 0
-Y <- as.matrix(Z>0)
-Y2 <- as.matrix(Z2>0)
+# conditional variance  z_i | z_{-i}
+dsig <- 1/sige * (ones + rho * rho * W2diag)  # SW: Sollte es nicht ones + rho * rho * W2diag sein?
+zvar <- ones/dsig;            # conditional variances for each z_i | z = H_{ii}^{-1} = 1/diag(H) (n x 1)
+                              # TODO: Was passiert im Fall dsig < 0 --> zvar < 0 (negative variance) !!!
+all.equal(zvar, 1 / diag(H))    # TRUE zvar = diag(H)^{-1}
 
-# Teste Simulation von z | y, beta, rho, sige ~ TMVN(Xbeta, sige^2*AI'AI, lower, upper)
-# Bsp. z[1] < 0, z[2] > 0 --> y[1]=0, y[2]=1
-lower<-c(-Inf,0)
-upper<-c(0, Inf)
-Z3 <- rtmvnorm(n=10000, mean=as.numeric(X%*%beta), sigma=Sigma, lower=lower, upper=upper)
+# conditional mean  z_i | z_{-i}
+mu <- X %*% beta
+zmu <- z - mu
+A  <- (1/sige)* S %*% zmu     # a vector (n x 1)
+B2  <- t(S) %*% A             # B2 <- (1/sige) * t(S) %*% S %*% zmu
+Cz <- zmu - zvar*B2           # Cz = (z - mu) - diag(H)^{-1} * H * (z - mu)
+zm <- mu + Cz;                # mu + (z-mu) - zvar * t(S)[ (1/sige) * S * (z - mu) ]  =  mu + (z-mu)
+# conditional mean based on H
+zm2 <- mu - 1 / diag(H) * H %*% zmu  + zmu
+all.equal(zm, zm2)          # TRUE
 
-par(mfrow=c(2,2))
-plot(density(Z3[,1]))
-ind <- Y2[,1]==0&Y2[,2]==1
-lines(density(Z2[ind,1]), col="red")
-# --> passt
+
+# sampled based on univariate truncated normal
+z1 <- matrix(NA, 100, n)
+# sampled based on multivariate truncated normal
+z2 <- matrix(NA, 100, n)
+for (i in 1:100) {
+  z1[i,ind0] <- rtnorm(mu=zm[ind0], sd=sqrt(zvar[ind0]), a=-Inf, b=0)
+  z1[i,ind1] <- rtnorm(mu=zm[ind1], sd=sqrt(zvar[ind1]), a=0, b=Inf)
+  
+  
+  z2[i,] <- rtmvnorm.sparseMatrix(n=1, mean=mu, H=H, lower=lower, upper=upper,
+                      start.value=as.double(z), burn.in=10)
+}
+
+plot(ecdf(z1[,1]))
+lines(ecdf(z2[,1]), col="red")
+
+plot(ecdf(z1[,2]))
+lines(ecdf(z2[,2]), col="red")
+
+
+
+
